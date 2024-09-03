e6f3535f2011a93543230f30d0401c0ead9148d4
Albert Krewinkel <albert@zeitkraut.de>
2024-09-03 14:50:54 +0200

    Separate traversal of lists from calling the filter function

--- jog.lua
+++ jog.lua
@@ -13,6 +13,14 @@ local function ptype (x)
   return mt and mt.__name:gsub('pandoc ', '') or type(x)
 end
 
+--- Checks whether the object is a list type.
+local function is_listy (x)
+  local tp = ptype(x)
+  return tp == 'Blocks'
+    or tp == 'Inlines'
+    or tp == 'List'
+end
+
 --- Function to traverse the pandoc AST.
 local jog
 
@@ -33,6 +41,7 @@ local function run_filter_function (fn, element, context)
   end
 end
 
+--- Set of Block and Inline tags that are leaf nodes.
 local leaf_node_tags = {
   Code = true,
   CodeBlock = true,
@@ -46,6 +55,7 @@ local leaf_node_tags = {
   Str = true,
 }
 
+--- Set of Block and Inline tags that have nested items in `.contents` only.
 local content_only_node_tags = {
   -- Blocks with Blocks content
   BlockQuote = true,
@@ -75,11 +85,42 @@ local content_only_node_tags = {
   Note = true,
 }
 
+--- Partially applied version of `jog`.
+local apply_jog = function (filter, context)
+  return function (element)
+    return jog(element, filter, context)
+  end
+end
+
+--- Concatenate all items into the given target table.
+local function concat_into(items, target)
+  -- unset all numerical indices
+  local orig_len = #target
+  local pos = 0
+  for _, sublist_or_element in ipairs(items) do
+    if is_listy(sublist_or_element) or
+       ptype(sublist_or_element) == 'table' then
+      for _, element in ipairs(sublist_or_element) do
+        pos = pos + 1
+        target[pos] = element
+      end
+    else
+      pos = pos + 1
+      target[pos] = sublist_or_element
+    end
+  end
+  -- unset remaining indices if the new list is shorter than the old
+  for i = pos + 1, orig_len do
+    target[i] = nil
+  end
+end
+
+--- Apply the filter on the nodes below the given element.
 local function recurse (element, filter, context)
   local tag = element.tag
   local tp = ptype(element)
   if leaf_node_tags[tag] then
-    -- do nothing, can traverse any deeper
+    -- do nothing, cannot traverse any deeper
   elseif content_only_node_tags[tag] or tp == 'Cell' then
     element.content = jog(element.content, filter, context)
   elseif tag == 'Image' then
@@ -96,48 +137,16 @@ local function recurse (element, filter, context)
     element.cells    = jog(element.cells, filter, context)
   elseif List{'TableHead', 'TableFoot'}:includes(tp) then
     element.rows    = jog(element.rows, filter, context)
+  elseif is_listy(element) then
+    local results = element:map(apply_jog(filter, context))
+    concat_into(results, element)
+  elseif tp == 'Pandoc' then
+    element.meta = jog(element.meta, filter, context)
+    element.blocks = jog(element.blocks, filter, context)
   else
     error("Don't know how to traverse " .. (element.t or tp))
   end
   return element
-end
-
---- Checks whether the object is a list type or a plain table
-local function is_listish (x)
-  local tp = ptype(x)
-  return tp == 'Blocks'
-    or tp == 'Inlines'
-    or tp == 'List'
-    or tp == 'table'
-end
-
---- Concatenate all items into the given target table.
-local function concat_into(items, target)
-  -- unset all numerical indices
-  local orig_len = #target
-  local pos = 0
-  for _, sublist_or_element in ipairs(items) do
-    if is_listish(sublist_or_element) then
-      for _, element in ipairs(sublist_or_element) do
-        pos = pos + 1
-        target[pos] = element
-      end
-    else
-      pos = pos + 1
-      target[pos] = sublist_or_element
-    end
-  end
-  -- unset remaining indices if the new list is shorter than the old
-  for i = pos + 1, orig_len do
-    target[i] = nil
-  end
-end
-
---- Partially applied version of `jog`; 
-local apply_jog = function (filter, context)
-  return function (element)
-    return jog(element, filter, context)
-  end
 end
 
 local non_joggable_types = {
@@ -160,36 +169,28 @@ jog = function (element, filter, context)
       element[key] = jog(value, filter, context)
     end
     result = element
+  elseif is_listy(element) then
+    element = recurse(element, filter, context)
+    result = run_filter_function(filter[tp], element, context)
   elseif tp == 'Block' then
     element = recurse(element, filter, context)
     local fn = filter[element.t] or filter.Block
     result = run_filter_function(fn, element, context)
-  elseif tp == 'Blocks' then
-    element = element:map(apply_jog(filter, context))
-    result = run_filter_function(filter.Blocks, element, context)
   elseif tp == 'Inline' then
     element = recurse(element, filter, context)
     local fn = filter[element.t] or filter.Inline
     result = run_filter_function(fn, element, context)
-  elseif tp == 'Inlines' then
-    local results = element:map(apply_jog(filter, context))
-    concat_into(results, element)
-    result = run_filter_function(filter.Inlines, element, context)
-  elseif tp == 'List' then
-    element = element:map(apply_jog(filter, context))
-    result = run_filter_function(filter.List,element, context)
   elseif tp == 'Meta' then
     for key, value in pairs(element) do
       element[key] = jog(value, filter, context)
     end
     result = run_filter_function(filter.Meta, element, context)
   elseif tp == 'Pandoc' then
-    element.meta = jog(element.meta, filter, context)
-    element.blocks = jog(element.blocks, filter, context)
+    element = recurse(element, filter, context)
     result = run_filter_function(filter.Pandoc, element, context)
   elseif List{'TableHead', 'TableFoot', 'Row', 'Cell'}:includes(tp) then
     element = recurse(element, filter, context)
-    result = run_filter_function(filter[tp:gsub('^pandoc ', '')], element, context)
+    result = run_filter_function(filter[tp], element, context)
   else
     warn("Don't know how to handle element ", tostring(element),
          ' of type ', tp, '\n')
