tarleb/jog

commit 90d5840c9c80b6ee2da2d7d30a811e28d6c05047

Adjust return types only when necessary

jog.lua
@@ -36,12 +36,6 @@ local function run_filter_function (fn, element, context)
   local result = fn(element, context)
   if result == nil then
     return element
-  end
-  local tp = ptype(result)
-  if tp == 'Inline' then
-    return pandoc.Inlines{result}
-  elseif tp == 'Block' then
-    return pandoc.Blocks{result}
   else
     return result
   end
@@ -122,29 +116,36 @@ local function recurse (element, filter, context, tp)
   elseif tp == 'TableHead' or tp == 'TableFoot' then
     element.rows    = jog(element.rows, filter, context)
   elseif tp == 'Blocks' or tp == 'Inlines' then
+    local expected_itemtype = tp == 'Inlines' and 'Inline' or 'Block'
     local pos = 0
-    local item_index = 1
+    local filtered_index = 1
     local filtered_items = element:map(function (x)
         return jog(x, filter, context)
     end)
-    local sublist_or_element = filtered_items[item_index]
-    while sublist_or_element ~= nil do
-      local tp = ptype(sublist_or_element)
-      if listy_type[tp] or tp == 'table' then
-        local subelement_index = 1
-        local subsubelement = sublist_or_element[subelement_index]
-        while subsubelement ~= nil do
+    local item = filtered_items[filtered_index]
+    local itemtype
+    while item ~= nil do
+      itemtype = ptype(item)
+      if itemtype ~= expexted_itemtype then
+        item = pandoc[tp](item)
+        itemtype = tp
+      end
+      if itemtype == tp then
+        local sublist_index = 1
+        local sublistitem = item[sublist_index]
+        while sublistitem ~= nil do
           pos = pos + 1
-          element[pos] = subsubelement
-          subelement_index = subelement_index + 1
-          subsubelement = sublist_or_element[subelement_index]
+          element[pos]  = sublistitem
+          sublist_index = sublist_index + 1
+          sublistitem   = item[sublist_index]
         end
       else
+        -- not actually a sublist, just an element
         pos = pos + 1
-        element[pos] = sublist_or_element
+        element[pos] = sublist
       end
-      item_index = item_index + 1
-      sublist_or_element = filtered_items[item_index]
+      filtered_index = filtered_index + 1
+      item = filtered_items[filtered_index]
     end
     -- unset remaining indices if the new list is shorter than the old
     pos = pos + 1

test/jog-inlines.lua
@@ -100,5 +100,13 @@ describe('Inline traversal', function()
       local expected = pandoc.Inlines('hello hello')
       assert.equals(expected, result)
     end)
+
+    it('works if the Inline function returns a string', function ()
+      local fn = function (str)
+        return str.text
+      end
+      local result = jog(pandoc.Inlines('eins zwo'), {Str = fn})
+      assert.equals(pandoc.Inlines('eins zwo'), result)
+    end)
   end)
 end)

commit 52f4311331f4dbb5cb9b22c5ee8f43e7cd329d79

Fix a performance regression introduced in 90d5840

jog.lua
@@ -123,7 +123,8 @@ local function recurse (element, tp, jogger)
     local itemtype
     while item ~= nil do
       itemtype = ptype(item)
-      if itemtype ~= expexted_itemtype then
+      if itemtype ~= tp and itemtype ~= expected_itemtype then
+        -- neither the list type nor the list's item type. Try to convert.
         item = pandoc[tp](item)
         itemtype = tp
       end
@@ -139,7 +140,7 @@ local function recurse (element, tp, jogger)
       else
         -- not actually a sublist, just an element
         pos = pos + 1
-        element[pos] = sublist
+        element[pos] = item
       end
       filtered_index = filtered_index + 1
       item = filtered_items[filtered_index]